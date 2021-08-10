3f82bd368be6bdf173e6de78814ca1fb70b3a248
DilanGiffu <52552836+DilanGiffu@users.noreply.github.com>
2021-08-10 11:28:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function nlogoref() {
+    location.href = "index.html"
+}
+
+function wref() {
+    open("https://api.whatsapp.com/send?phone=5511992097987&text=Bem-vindo%20a%20Novel!!%20Conta%20com%20a%20gente.")
+}
+
+function iref() {
+    open("https://instagram.com/novel.oficial")
+}
